e04e5e7638fb9aecaea59c215807015a3012b182
adibs0132 <andrew0132@gmail.com>
2017-09-13 11:32:44 -0400

    Added Title and backstory

--- zork.py
+++ zork.py
@@ -3,6 +3,18 @@
 #Date: 9 September 2017
 
 def main():
+
+    #Title
+    print("\t\t\tRunaway Train\n"
+          "\t\t\t=============\n")
+    #Back Story
+    print("You are a roller coaster enthusiast and you finally found the opportunity\n"
+          "to go to Six flags to ride the record breaking King da Ka.\n"
+          "After a 3 hour long car ride you can finally see the monstrous ride and\n"
+          "feel a little worried and you think to yourself\n\n"
+          "Maybe the runaway train is a better option...\n")
+
+    #Instance Variables          
     entrance= "You are at the Entrance of an amusement park"
     ferrisWheel= "You are at the Ferris Wheel"
     bridge = "You are sitting on the bridge"
