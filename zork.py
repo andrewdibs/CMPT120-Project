c57ba1164e3a57abf21b54c7f59b3139c2d86410
andrewdibs <Andrew.DiBella1@marist.edu>
2017-10-19 21:42:36 -0400

    Bugfixes and score only appears when menu command is used

--- zork.py
+++ zork.py
@@ -44,21 +44,10 @@ def main():
     #Start of Game
         titleIntro()
         character = setCharacter()
-
-    #Starting character chosen print statement
-        if character == "Jon Snow":
-            print("You are Jon Snow, King of the North.")
-        if character == "Jaime":
-            print("You are Jaime Lannister, deadly even with one hand.\n")
-        if character == "Arya":
-            print("You are Arya Stark, I worry for anyone who gets in your way.\n")
-        if character == "Tyrion":
-            print("You are the dwarf Tyrion Lannister, you drink and know things\n")
             
-
         while True:
 
-            if countCasBlack == 0:
+            if countHasBeen[0] == 0:
                     print(character,"!!!WAKE UP! We have to get the hell out "
                           "of here. Where should we go?")
                     
@@ -86,12 +75,12 @@ def main():
                 
         #Castle Black
             if curLocation == location[0]:
-                visitCasBlack = True
+                hasBeenThere[0] = True
 
-                if countCasBlack < 1:
+                if countHasBeen[0] < 1:
                     score += 5
 
-                countCasBlack += 1
+                countHasBeen[0] += 1
                 
                 if command == "north":
                     curLocation = location[5]
@@ -105,12 +94,12 @@ def main():
                     
         #North of Wall
             elif curLocation == location[5]:
-                visitNorthWall = True
+                hasBeenThere[5] = True
 
-                if countNorthWall < 1:
+                if countHasBeen[5] < 1:
                     score += 5
 
-                countNorthWall += 1
+                countHasBeen[5] += 1
 
                 if score < 25:
                     print("Why the hell would you go beyond the wall..")
@@ -121,14 +110,14 @@ def main():
 
         #Winterfell
             elif curLocation == location[1]:
-                visitWinterfell = True
+                hasBeenThere[1] = True
 
-                if countWinterfell < 1:
+                if countHasBeen[1] < 1:
                     score += 5
 
-                countWinterfell += 1
+                countHasBeen[1] += 1
 
-                if countWinterfell == 1:
+                if countHasBeen[1] == 1:
                     sansaQues= input("You are greeted by Sansa Stark..\n Sansa: "+character +
                                      ", will you help us win this war?\nYes or no: ").lower()
                     if sansaQues == "yes":
@@ -148,12 +137,12 @@ def main():
 
         #Kings Landing
             elif curLocation == location[2]:
-                visitKingsLanding = True
+                hasBeenThere[2] = True
 
-                if countKingsLanding < 1:
+                if countHasBeen[2] < 1:
                     score += 5
 
-                countKingsLanding +=1
+                countHasBeen[2] +=1
 
                 if command == "north":
                     curLocation = location[3]
@@ -165,12 +154,12 @@ def main():
                     
         #Dragonstone                   
             elif curLocation == location[3]:
-                visitDragonstone = True
+                hasBeenThere[3] = True
 
-                if countDragonstone < 1:
+                if countHasBeen[3] < 1:
                     score += 5
 
-                countDragonstone += 1
+                countHasBeen[3] += 1
                 
                 if command == "south":
                     curLocation = location[2]
@@ -182,19 +171,19 @@ def main():
 
          #Iron Islands                   
             elif curLocation == location[4]:
-                visitIronIslands = True
+                hasBeenThere[4] = True
 
-                if countIronIslands == 1:
+                if countHasBeen[4] == 1:
                     score += 5
 
-                countIronIslands += 1
+                countHasBeen[4] += 1
 
                 if command == "east":
                     curLocation = location[1]
                 else:
                     print("Invalid command.")
 
-            scoreLocation(curLocation, score)
+            printLocation(curLocation)
 
     
     def titleIntro():
@@ -219,18 +208,22 @@ def main():
         
             if character[0:3] == "jon":
                 character = "Jon Snow"
+                print("You are Jon Snow, King of the North.\n")
                 break
             
             if character[0:3] == "ary":
                 character = "Arya"
+                print("You are Arya Stark, I worry for anyone who gets in your way.\n")
                 break
             
             if character[0:3] == "tyr":
                 character = "Tyrion"
+                print("You are the dwarf Tyrion Lannister, you drink and know things\n")
                 break
             
             if character[0:3] == "jai":
                 character = "Jaime"
+                print("You are Jaime Lannister, deadly even with one hand.\n")
                 break
             
             else:
@@ -248,9 +241,9 @@ def main():
         return menu
         
           
-    def scoreLocation(curLocation, score):
-        print(curLocation)
-        print("Score: ", score, "\n")
+    def printLocation(curLocation):
+        print(curLocation, "\n")
+        
         
     
     def conclusion():
