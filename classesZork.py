ba075ff4b71af19cbd8e6809f0a7c3985c33a084
andrewdibs <Andrew.DiBella1@marist.edu>
2017-11-23 10:28:39 -0500

    Created new file for classes

---
+++ classesZork.py
@@ -0,0 +1,43 @@
+#classesZork.py
+#Author Andrew DiBella
+#Date 24 November 2017
+
+class Player:
+
+
+    def __init__(self, name, moves ):
+        
+        self.name = name
+        self.moves = moves
+        self.inventory = []
+        self.curLoc = 0
+
+    def move():
+        pass
+
+
+    def take():
+        pass
+
+
+    def drop():
+        pass
+
+
+    def look():
+        pass
+
+
+    def attack():
+        pass
+
+class Locale:
+
+    def __init__(self, name, beforeVist, afterVisit, item):
+
+        self.name = name
+        self.beforeVisit= beforeVisit
+        self.afterVisit = afterVisit
+        self.item = item
+        self.visited = False
+        self.searched = False
